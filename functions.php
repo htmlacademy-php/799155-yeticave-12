21be014754fe918919360c52807635c101f6dd9c
Holerik <a_zobnin@mail.ru>
2021-05-01 15:07:35 +0300

    Работаем с датой

--- functions.php
+++ functions.php
@@ -1,6 +1,45 @@
 <?php
+/**
+ * Форматирует цену в рублях, отделяя каждые три знака цены пробелом
+ * Добавляет в конец строки символ рубля
+ *
+ * @param $price цена товара в рублях
+ *
+ * @return $formatted_price Форматированная строка с ценой
+ */
+
 function format_price(float $price) : string {
     $formatted_price = number_format(ceil($price), 0, null, " ") . "₽";
     return $formatted_price;
+}
+
+/**
+ * Возвращает количество целых часов и остатка минут до даты из будущего в формате «ЧЧ:ММ»
+ * 
+ * @param $time дата из будущего в формате ГГГГ-ММ-ДД
+ * @return [$hours, $mins, $s_hours, $s_mins] количество целых часов и остатка минут и их строковый вариант
+ */
+
+function get_time_left(string $time)  {
+    if (!is_date_valid($time)) {
+        return false;
+    }
+    $now_time = strtotime('now');
+    $fin_time = strtotime($time);
+    if ($fin_time <= $now_time) {
+        return false;
+    }
+    $diff = $fin_time - $now_time;
+    $hours = floor($diff / 3600);
+    $mins = floor($diff % 3600 / 60);
+    $s_hours = strval($hours);
+    if ($hours < 10) {
+        $s_hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    }
+    $s_mins = strval($mins);
+    if ($mins < 10) {
+        $s_mins = str_pad($mins, 2, "0", STR_PAD_LEFT);
+    }
+    return [$hours, $mins, $s_hours, $s_mins];
 }
 ?>
