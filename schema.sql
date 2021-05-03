077e9643022070927996c75360ef752a2cd2fa8c
Holerik <a_zobnin@mail.ru>
2021-05-03 13:31:41 +0300

    Создаём схему БД личного проекта

---
+++ schema.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE YetiCave DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+USE YetiCave;
+
+CREATE TABLE cats (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  code VARCHAR(64) NOT NULL
+);
+
+CREATE INDEX code_ind ON cats(code);
+
+CREATE TABLE lots (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dt_add DATETIME NOT NULL,
+  name VARCHAR(128) NOT NULL UNIQUE,
+  descr TEXT NOT NULL,
+  img_url VARCHAR(256) NOT NULL UNIQUE,
+  price INT NOT NULL,
+  dt_expired DATE NOT NULL,
+  bet_step INT NOT NULL,
+  author_id INT NOT NULL,
+  winnwer_id INT DEFAULT NULL,
+  cat_id INT NOT NULL
+);
+
+CREATE INDEX cat_ind ON lots(cat_id);
+
+CREATE TABLE bets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dt_add DATETIME NOT NULL,
+  price INT NOT NULL,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL
+);
+
+CREATE INDEX user_ind ON bets(user_id);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dt_reg DATETIME NOT NULL,
+  email VARCHAR(64) NOT NULL UNIQUE,
+  name VARCHAR(64) NOT NULL UNIQUE,
+  password VARCHAR(64) NOT NULL UNIQUE,
+  contact TEXT NOT NULL,
+  bets_id VARCHAR(64) DEFAULT NULL,
+  lots_id VARCHAR(64) DEFAULT NULL
+);
+
+CREATE INDEX email_ind ON users(email);
