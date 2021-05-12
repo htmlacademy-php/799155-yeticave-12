62e4d57c0163e73d1b6c5dc54f8ca7e5fc9976fb
Holerik <a_zobnin@mail.ru>
2021-05-12 19:44:37 +0300

    Добавил в sql запрос отдельного лота dt_expired > NOW()

--- Repository.php
+++ Repository.php
@@ -37,7 +37,7 @@ class Repository extends Database {
     $lots = array();
     if ($this->isOk()) {
       $sql = "SELECT l.id, l.name, descr, price, img_url, dt_add, dt_expired, c.name as cat_name, cat_id" . 
-      " FROM lots l JOIN cats c ON c.id = l.cat_id WHERE l.id = $id";
+      " FROM lots l JOIN cats c ON c.id = l.cat_id WHERE l.id = $id AND dt_expired > NOW()";
       $result = $this->query($sql);
       if ($this->isOk()) {
           $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
